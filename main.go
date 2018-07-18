470eac9d4c7d09508b37f7fcef775a4d334f007f
John Hilliard <praetoriansentry@gmail.com>
2018-07-18 06:51:35 +0000

    minor logging changes

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/richardlehane/mscfb"
-	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+
+	"github.com/richardlehane/mscfb"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
@@ -17,28 +17,28 @@ var (
 )
 
 func main() {
+	log.Print("Started")
 	kingpin.Version("0.0.1")
 	kingpin.Parse()
 	doc, err := mscfb.New(*cfbffile)
 	kingpin.FatalIfError(err, "Failed to parse file")
+	log.Print("Read file")
 	dir, err := ioutil.TempDir(".", "cfbfdump-")
 	kingpin.FatalIfError(err, "Cant create temp directory")
-
+	log.Printf("Creating output directory: %s", dir)
 	for entry, err := doc.Next(); err == nil; entry, err = doc.Next() {
 		p := strings.Join(entry.Path, string(os.PathSeparator))
 		fullPath := dir + string(os.PathSeparator) + p
 		_ = os.Mkdir(fullPath, os.ModePerm)
-
 		buf := make([]byte, entry.Size)
 		i, _ := doc.Read(buf)
 		if i > 0 {
 			ioutil.WriteFile(fullPath+string(os.PathSeparator)+entry.Name, buf, os.ModePerm)
-			log.Println(entry.Name)
-			asdf, _ := json.Marshal(entry)
-			fmt.Println(string(asdf[:]))
+			entryData, _ := json.Marshal(entry)
+			log.Printf("Writing entry: %s", string(entryData[:]))
 
 		}
 
 	}
-	log.Print("started")
+
 }
